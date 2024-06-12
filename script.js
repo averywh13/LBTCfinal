ad5f0d45103e07968591cd1498f3011e97a3eb2e
averywh13 <awekslerhart@gmail.com>
2024-06-12 14:57:52 -0400

    added message notification for when it form sends

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// toast JS
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
 
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}
 
 
